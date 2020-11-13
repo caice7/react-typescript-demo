cf93b340ca73fc509939fe758bef86791e6f5e1b
caice <shineyangguang@126.com>
2020-11-13 15:30:42 +0800

    init

--- public/configs.js
+++ public/configs.js
@@ -1,8 +1,2 @@
 window.SYSTEM_CONFIGS = {
-  list: [
-    { pic: "yth.png", name: "一体化地图服务", url: "http://localhost:8080/OneMapCesium/" },
-    { pic: "sq.png", name: "社区管理", url: "http://10.0.10.164:8888/OneMapCesium/" },
-    { pic: "dsj.png", name: "时空大数据", url: "http://10.0.10.164:8888/OneMap/" },
-    { pic: "gis.png", name: "视频GIS", url: "http://10.0.10.164:8888/OneMapCesium/" },
-  ]
 }
